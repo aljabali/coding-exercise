9e54a8ac12e52cd5143ca8adb3f4566c5bafcd71
aljabali.fadi@gmail.com <aljabali.fadi@gmail.com>
2018-03-10 15:49:29 +0200

    fix build

--- src/test/java/com/aljabali/exercise/CodingExerciseApplicationTests.java
+++ src/test/java/com/aljabali/exercise/CodingExerciseApplicationTests.java
@@ -1,5 +1,6 @@
 package com.aljabali.exercise;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -7,6 +8,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CodingExerciseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
 
 
 }
